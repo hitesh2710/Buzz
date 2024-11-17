d7576f76d59f681f92f4c2be1378d94278b8e729
Hiteshwarm Dubey <87112296+hitesh2710@users.noreply.github.com>
2024-11-18 02:07:27 +0530

    Update sendTokenAndCookies.js

--- backend/util/sendTokenAndCookies.js
+++ backend/util/sendTokenAndCookies.js
@@ -10,6 +10,7 @@ const sendTokenAndCookies=(user,statusCode,res)=>{
             Date.now() + process.env.cookieExpire*24*60*60*1000
         ),
         httpOnly:true,
+        secure:true,
     }
 
     res.status(statusCode).cookie('token',token,options).json({
